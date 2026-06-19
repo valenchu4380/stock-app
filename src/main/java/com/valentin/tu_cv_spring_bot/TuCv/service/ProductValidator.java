cafa8f4a2653df38cd75d12bd73ea9ebd89fd248
valentin <valenchu4380@gmail.com>
2026-06-18 23:30:24 -0300

    fix lista productos al cargar

--- src/main/java/com/valentin/tu_cv_spring_bot/TuCv/service/ProductValidator.java
+++ src/main/java/com/valentin/tu_cv_spring_bot/TuCv/service/ProductValidator.java
@@ -5,10 +5,10 @@ import com.valentin.tu_cv_spring_bot.TuCv.mODEL.Product;
 
 public class ProductValidator {
     public static void validate(Product product) throws InvalidProductException{
-        if(product.getPrice()<=0){
+        if(product.getPrice()<0){
             throw new InvalidProductException("El price debe ser mayor a 0");
         }
-        if(product.getStock()<=0){
+        if(product.getStock()<0){
             throw new InvalidProductException("El stock del producto no puede ser negativo");
         }
     }
